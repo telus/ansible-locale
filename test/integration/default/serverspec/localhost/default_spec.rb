f1f8d0ff13851650030e77a156d67403fee92159
Steven Harradine <stevenharradine@gmail.com>
2016-03-17 21:18:12 -0400

    fix included text case and whatnot

--- test/integration/default/serverspec/localhost/default_spec.rb
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe 'ansible-locale::default' do
 
   describe command('locale -a') do
-    its(:stdout) { should include("en_CA.UTF-8") }
-    its(:stdout) { should include("fr_CA.UTF-8") }
+    its(:stdout) { should include("en_CA.utf8") }
+    its(:stdout) { should include("fr_CA.utf8") }
   end
 
 end
